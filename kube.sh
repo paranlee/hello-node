a9af0572de5b0085eb8d9ecf42386b5fdebd4bb6
paranlee <paran.lee@mail.uk>
2021-02-05 16:28:26 +0900

    install kube bash shell script

---
+++ kube.sh
@@ -0,0 +1,12 @@
+# kubectl
+sudo apt update -y && sudo apt install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt update -y && sudo apt install -y kubectl
+
+# helm
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update -y && sudo apt-get install -y helm
+
